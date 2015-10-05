1d713c866795f3ac3307cb9322ffc4c8cb59b1fa
Kristie DeLouise <kdelouise@gmail.com>
2015-10-04 21:13:25 -0400

    migration of repo

--- js/site.js
+++ js/site.js
@@ -1,13 +1,16 @@
 $(function () {
 
   $('nav ul li').click(function () {
-
     $('.content').children().each(function () {
       $(this).toggle(false);
     });
 
     $('#' + this.classList[0]).toggle(true);
-    console.log(this.classList[0]);
+    if(this.classList[0] == "about") {
+      $('.parallax-mirror').toggle(true);
+    } else {
+      $('.parallax-mirror').toggle(false);
+    }
   });
 
 });
